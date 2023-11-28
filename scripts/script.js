c927783d273a5cec68195877819ddc59f1618316
DarthEvgenius <clay14@yandex.ru>
2023-11-28 22:55:10 +0300

    resipe small

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+// burger menu
+document.addEventListener('DOMContentLoaded', ()=> {
+    const burger = document.querySelector('.menu_burger');
+    const menu = document.querySelector('.header__menu');    
+    const closeMenu = document.querySelector('.menu_close');
+    
+    if (burger) {
+        console.log(burger);
+        closeMenu.addEventListener('click', ()=> {
+            menu.classList.remove('active');
+        });
+        burger.addEventListener('click', ()=> {
+            menu.classList.add('active');             
+        });
+    }
+        
+})
